52f2d6ddd79e825537299ce037a9b8d4724d05bc
root <root@LAPTOP-U5112P1U.localdomain>
2021-05-14 17:06:14 +0100

    progress

---
+++ routes/task-routes.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const Task = require("../models/task-model");
+const User = require('../models/user-model');
+
+//Get all Tasks
+router.get("/tasks", async (req, res) => {
+  try {
+    
+    const allTasks = await Task.find();
+    res.status(200).json(allTasks);
+    
+  } catch (e) {
+    res.status(500).json(`error occurred ${e}`);
+  }
+});
+
+//Create Task
+router.post("/tasks", async (req, res) => {
+  const { title } = req.body;
+  if (!title) {
+    res.status(400).json("missing fields");
+    return;
+  }
+
+  try {
+    const response = await Task.create({
+      title
+    });
+    res.status(200).json(response);
+  } catch (e) {
+    res.status(500).json(`error occurred ${e}`);
+  }
+});
+
+//Delete Task
+router.delete('/tasks/:id', async (req, res) => {
+  try {
+    await Task.findByIdAndRemove(req.params.id);
+    res.status(200).json(`Task with id ${req.params.id} deleted.`)
+  } catch(e) {
+    res.status(500).json(`error occurred ${e}`);
+  }
+});
+
+//get by Id
+router.get('/tasks/:id', async (req,res ) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    res.status(200).json(task);
+  } catch(e) {
+    res.status(500).json(`error occurred ${e}`);
+  }
+})
+
+// router.get('/weather', async(req, res) => {
+//   const weather = new Weather({
+//     appID: process.env.WEATHER_KEY
+// });
+
+//Update Task
+router.put('/tasks/:id', async (req, res) => {
+  try{
+  const {title} = req.body;
+  await Task.findByIdAndUpdate(req.params.id, { title})
+  res.status(200).json(`Task with id ${req.params.id} was updated.`);
+  } catch(e) {
+    res.status(500).json(`error occurred ${e}`);
+  }
+});
+
+
+module.exports = router;
